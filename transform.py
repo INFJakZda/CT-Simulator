a468d5220ccf99d71ed1ca4de8b951c8bdebc829
INFJakZda <zdanowski78@gmail.com>
2018-03-10 16:27:28 +0100

    Change radon transform

--- transform.py
+++ transform.py
@@ -7,15 +7,20 @@ from skimage.io import imread
 from skimage import data_dir
 from skimage.transform import radon, rescale
 from skimage.transform import iradon
+from scipy import misc
 
 filename = "./data/test.png"
+steps = 150
 
-def to_sinogram(image, theta):
-    #TODO radon
-    sinogram = radon(image, theta=theta, circle=True) 
-    return sinogram
+def radon_transform(image, theta, steps):
+    #transformed = radon(image, theta=theta, circle=True) 
+    transformed = np.zeros((steps, len(image)), dtype='float64')
+    for s in range(steps):
+        rotation = misc.imrotate(image, -s*180/steps).astype('float64')
+        transformed[:,s] = sum(rotation)
+    return transformed
 
-def from_sinogram(image, theta):
+def reverse_radon(image, theta):
     reconstruction_fbp = iradon(sinogram, theta=theta, circle=True)
     error = reconstruction_fbp - image
     print('FBP rms reconstruction error: %.3g' % np.sqrt(np.mean(error**2)))
@@ -39,7 +44,7 @@ def print_from_image(image, sinogram):
     fig.tight_layout()
     plt.show()
     
-def print_from_sinogram(reconstruction_fbp, theta):
+def print_reverse_radon(reconstruction_fbp, theta):
     imkwargs = dict(vmin=-0.2, vmax=0.2)
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5),
                                 sharex=True, sharey=True)
@@ -52,8 +57,8 @@ def print_from_sinogram(reconstruction_fbp, theta):
 if __name__ == '__main__':
     image = imread(filename, as_grey=True)
     theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-    sinogram = to_sinogram(image, theta)
+    sinogram = radon_transform(image, theta, steps)
     print_from_image(image, sinogram)
     
-    reconstruction = from_sinogram(sinogram, theta)
-    print_from_sinogram(reconstruction, theta)
+    reconstruction = reverse_radon(sinogram, theta)
+    print_reverse_radon(reconstruction, theta)
